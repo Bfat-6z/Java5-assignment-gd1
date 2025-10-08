bb12b8b4d6efe08802a34cb87f4cacd60a30df39
TranHoangQuan <tranquan567098@gmail.com>
2025-10-08 14:29:30 +0700

    Change informaion from main list admincontroller and render list product, dashbroad

--- Ass-SOF3022/src/main/java/poly/edu/asssof3022/controller/admin/AdminController.java
+++ Ass-SOF3022/src/main/java/poly/edu/asssof3022/controller/admin/AdminController.java
@@ -19,19 +19,27 @@ public class AdminController {
         return "admin/layout/main"; // Trả về layout chính
     }
 
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        System.out.println("DEBUG Dashboard: Method HIT OK!");  // Log này PHẢI in nếu fix đúng
+        model.addAttribute("title", "Bảng điều khiển Dashboard");
+        return "admin/dashboard";  // Path templates/admin/dashboard.ht
+    }
+
     // Trang danh sách sản phẩm
     @GetMapping("/products")
     public String showList(Model model) {
         List<Product> list = List.of(
+
                 new Product(1, "Bánh mì", 10000.0, 50),
                 new Product(2, "Sữa tươi", 15000.0, 20),
                 new Product(3, "Trà sữa", 30000.0, 12)
         );
-
         model.addAttribute("list", list);
         model.addAttribute("title", "Danh sách sản phẩm");
-
-
-        return "admin/product/list";
+        return "admin/product/list";  // Trả về view list.html
     }
+
+
+
 }
