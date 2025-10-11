6523ba7169ac3650f85f0c477a1bfaa7eb5411a8
TranHoangQuan <tranquan567098@gmail.com>
2025-10-11 07:51:28 +0700

    update to render dashboard

--- Ass-SOF3022/src/main/java/poly/edu/asssof3022/controller/admin/AdminController.java
+++ Ass-SOF3022/src/main/java/poly/edu/asssof3022/controller/admin/AdminController.java
@@ -1,19 +1,29 @@
 package poly.edu.asssof3022.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class AdminController {
 
     @GetMapping("/admin/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        model.addAttribute("totalProducts", 125);
+        model.addAttribute("totalCustomers", 56);
+        model.addAttribute("revenue", 34000000);
+
+        model.addAttribute("activeMenu", "dashboard");
         return "admin/dashboard";
     }
 
     @RequestMapping("/admin/products")
-    public String panel() {
+    public String products(Model model)
+    {
+        model.addAttribute("activeMenu", "products");
         return "admin/products";
     }
 }
